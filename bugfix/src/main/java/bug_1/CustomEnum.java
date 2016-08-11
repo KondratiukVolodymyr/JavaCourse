c210d63fc6e8f21f6379efced88b922906ba65f1
KondratiukVolodymyr <kvp1708@i.ua>
2016-08-11 12:10:15 +0300

    add to Git

---
+++ bugfix/src/main/java/bug_1/CustomEnum.java
@@ -0,0 +1,14 @@
+package bug_1;
+
+public enum CustomEnum {
+
+   ACTION_ONE(0),
+   ACTION_TWO(1),
+   ECTION_THREE(2);
+
+    int act;
+
+    CustomEnum(int act){
+        this.act = act;
+    }
+}
